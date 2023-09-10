6ca8da323fc64a4a7752e19697bbe8f157190a04
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-10 03:44:59 -0700

    var

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet in lowercase,
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while
+	{
+		putchar(ch);
+		ch++;
+	}
+	putcha('\n');
+
+	return (0);
+}
