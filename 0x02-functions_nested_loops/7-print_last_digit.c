d1e726237166d4678c90b9b20a5e3d063a8b5b53
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-13 12:41:39 -0700

    var

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,17 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int lastdigit;
 
 	if (n < 0)
 	{
 		n = -n;
 	}
 
-	last_digit = n % 10;
+	lastdigit = n % 10;
 
-	_putchar(last_digit + '0');
+	_putchar(lastdigit + '0');
 
-	return (last_digit);
+	return (lastdigit);
 
 }
