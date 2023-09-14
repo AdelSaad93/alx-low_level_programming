4648ad2936f739f6b737bb5d84e8d064b2217b9f
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-14 16:53:35 -0700

    var

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: Always 0
+ * Return: 0
  */
 
 int main(void)
@@ -16,10 +16,10 @@ int main(void)
 	/* while (i < 10) */
 	/* { */
 	/*     putchar(i); */
-	/*     i++; */
 	/* } */
 
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
+
