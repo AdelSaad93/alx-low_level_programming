81043f9c5903e54e7e17c9b6189806dd6ff7f451
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-14 17:14:32 -0700

    var

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,9 +10,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+		i = 0;
 	/**
-	*i = 0;
-	*
 	*while (i < 10)
 	*{
 	*     putchar(i);
@@ -23,3 +23,4 @@ int main(void)
 
 	return (0);
 }
+
