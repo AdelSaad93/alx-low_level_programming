b4d9e9b9e88ad88a79cb3af4fb12a9c90e9e4265
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-19 08:17:17 -0700

    var

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,14 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	if (a != NULL && n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", a[i]);
-			if (i < n - 1)
-				printf(", ");
-		}
-		printf("\n");
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
 	}
+	printf("\n");
 }
+
