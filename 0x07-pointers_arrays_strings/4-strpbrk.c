c7031865eb7be2e0e88435bf959e5dbbfc648bcc
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-09-26 10:52:38 -0700

    var

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,8 +5,8 @@
  * @s: Pointer to the string to be searched.
  * @accept: Pointer to the string containing bytes to search for.
  *
- * Return: Pointer to the first occurrence of any byte in s that matches one
- *         of the bytes in accept, or NULL if no such byte is found.
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
@@ -17,7 +17,6 @@ char *_strpbrk(char *s, char *accept)
 			if (*s == accept[i])
 				return (s);
 		}
-
 		s++;
 	}
 
