d591461b298e7ab8005c89375363eff2865ee213
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-04 03:21:06 -0700

    var

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string to be searched.
+ * @accept: Pointer to the string containing characters to search for.
+ *
+ * Return: The number of bytes in the initial segment of s that consist only
+ *         of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+		if (s[j] == '\0')
+			return (result);
+	}
+	return (result);
+}
+
+
