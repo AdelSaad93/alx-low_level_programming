880fa6872f6832b5ae9105e82387bf3a9f2fc320
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-10 22:26:54 -0700

    var

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point for the program
+ *
+ * This function serves as the entry point for the program.
+ * It prints the name of the source file.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
