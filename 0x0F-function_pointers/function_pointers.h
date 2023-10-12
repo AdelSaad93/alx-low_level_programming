5a6d456b06b3570c5c60d3c7608ed18cc74342be
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-12 06:34:52 -0700

    var

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,10 @@
-#ifndef FUNCTIONS_POINTERS_H
-#define FUNCTIONS_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <stddef.h>
 
-int _putchar(char c);
-
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
 
 #endif
