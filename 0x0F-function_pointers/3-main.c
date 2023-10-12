5f25ee925da6ce8cd1de51abddc70799f9d0b3a5
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-12 07:25:09 -0700

    var

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,12 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-
-	int i = 0;
-
-	while (ops[i].op != NULL)
-	{
-		if (*s == *(ops[i].op))
-			return (ops[i].f);
-		i++;
-	}
-
-	printf("Error\n");
-	exit(99);
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * main - main function
- * @argc: arguments
- * @argv: array of args
- * Return: return something
+ * main - Main function
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ * Return: 0 on success, error codes on failure
  */
 int main(int argc, char *argv[])
 {
@@ -44,6 +19,7 @@ int main(int argc, char *argv[])
 		a = atoi(argv[1]);
 		b = atoi(argv[3]);
 		func = get_op_func(argv[2]);
+
 		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
 		{
 			printf("Error\n");
@@ -55,3 +31,4 @@ int main(int argc, char *argv[])
 	printf("Error\n");
 	exit(98);
 }
+
