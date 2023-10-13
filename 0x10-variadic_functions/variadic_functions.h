3fcd71d54066cf5760dcb3e79a7022e499ac315c
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-13 12:28:56 -0700

    var

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,5 +11,12 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+void print_char(va_list args);
+
+void print_integer(va_list args);
+
+void print_float(va_list args);
+
+void print_string(va_list args);
 
 #endif
