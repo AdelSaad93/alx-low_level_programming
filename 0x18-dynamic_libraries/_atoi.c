7b502be0e74dd9c285f498e7ec1573e7a1c3229c
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-03-26 13:38:37 -0700

    var

--- 0x18-dynamic_libraries/_atoi.c
+++ 0x18-dynamic_libraries/_atoi.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include "_strlen.c"
 
 /**
  * _atoi - converts string to integer
--- 0x18-dynamic_libraries/_strcpy.c
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "_strlen.c"
+
 /**
  * _strcpy - main function to copy
  *
