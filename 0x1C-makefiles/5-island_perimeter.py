5b19ca0933c4932a22ac79183c3ddc40beba8e25
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-29 23:01:27 -0700

    var

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,45 +5,45 @@ This module defines a function to calculate the perimeter of an island.
 
 
 def island_perimeter(grid):
-	"""
-	Calculates the perimeter of the island described in grid.
+    """
+    Calculates the perimeter of the island described in grid.
 
-	Args:
-		grid (list): A list of list of integers representing the island grid.
+    Args:
+        grid (list): A list of list of integers representing the island grid.
 
-	Returns:
-		int: The perimeter of the island.
+    Returns:
+        int: The perimeter of the island.
 
-	Example:
-		grid = [
-			[0, 0, 0, 0, 0, 0],
-			[0, 1, 0, 0, 0, 0],
-			[0, 1, 0, 0, 0, 0],
-			[0, 1, 1, 1, 0, 0],
-			[0, 0, 0, 0, 0, 0]
-		]
-		island_perimeter(grid)  # Output: 12
-	"""
-	perimeter = 0
+    Example:
+        grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        island_perimeter(grid)  # Output: 12
+    """
+    perimeter = 0
 
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			if grid[i][j] == 1:
-				perimeter += 4
-				if i > 0 and grid[i - 1][j] == 1:
-					perimeter -= 2
-				if j > 0 and grid[i][j - 1] == 1:
-					perimeter -= 2
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-	return perimeter
+    return perimeter
 
 
 if __name__ == "__main__":
-	grid = [
-		[0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 0, 0],
-		[0, 1, 0, 0, 0, 0],
-		[0, 1, 1, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0]
-	]
-	print(island_perimeter(grid))
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
